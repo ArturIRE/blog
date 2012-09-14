1a32a18de97e234a0931a1407df026df7cf15e1a
ArturPhilibin <dasilva.artur@gmail.com>
2012-09-14 11:11:16 +0100

    Initial files created by 'rails new .'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f2347d9749c5637c52f1f66919ea047ca105960872127df3b9b5943a364566426101ff0b6ac9bc204e91337ed04f856e36f8099db69dd88ac787009d10e26b6a'
